16bfa747e57d1261a5501c8bbfd7928e012d694c
Hartmut Holzgraefe <hartmut@php.net>
2017-07-28 10:43:40 +0200

    A very first upload form that just prints the raw unparsed contents of the uploaded gdb log for now

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+ <head>
+  <title>GDB backtrace parser</title>
+ </head>
+ <body>
+<?php
+if (empty($_FILES)) {
+    // nothing uploaded yet -> show upload form and instructions
+?>
+ <h1>GDB log parser</h1>
+
+ <p>
+    Parses output of <tt style='background-color: lightblue'>gdb&gt; thread apply bt all</tt>
+    from either a <tt>core</tt> file or a running MariaDB/MySQL server instance .
+ </p>
+
+ <p>
+    You can either upload your own <tt>gdb</tt> log file, or just press the [Send file] button
+    without selecting any file at all to see a demo.
+    
+ <form enctype="multipart/form-data" method="POST">
+    GDB backtrace: <input name="gdb" type="file" />
+    <br/>
+    <input type="submit" value="Send File" />
+</form>    
+<?php
+} else {
+    // take uploaded file, or bundled "gdb.log" example if no file was selected
+    $gdbfile = $_FILES['gdb']['tmp_name'] ?: "./gdb.log";
+
+    echo "<pre>\n";
+    readfile($gdbfile);
+    echo "</pre>\n";
+}
+?>
+ </body>
+</html>
